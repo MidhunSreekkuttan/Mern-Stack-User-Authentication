8441a6f050b5e9e44f8b837f5ba42bd19c755e88
MidhunSreekkuttan <MidhunSreekkuttan987@gmail.com>
2025-11-23 11:36:19 +0530

    Add a file to the server

---
+++ server/controller/emailTemplate.js
@@ -0,0 +1,138 @@
+export const welcomeTemplate = `
+
+<html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+  <head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title></title>
+    <!--[if (mso 16)]>
+    <style type="text/css">
+    a {text-decoration: none;}
+    </style>
+    <![endif]-->
+    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]-->
+    <!--[if gte mso 9]>
+<noscript>
+         <xml>
+           <o:OfficeDocumentSettings>
+           <o:AllowPNG></o:AllowPNG>
+           <o:PixelsPerInch>96</o:PixelsPerInch>
+           </o:OfficeDocumentSettings>
+         </xml>
+      </noscript>
+<![endif]-->
+    <!--[if !mso]><!-- -->
+    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@500&display=swap" rel="stylesheet">
+    <!--<![endif]-->
+    <!--[if mso]><xml>
+    <w:WordDocument xmlns:w="urn:schemas-microsoft-com:office:word">
+      <w:DontUseAdvancedTypographyReadingMail/>
+    </w:WordDocument>
+    </xml><![endif]-->
+  </head>
+  <body class="body">
+    <div dir="ltr" class="es-wrapper-color">
+      <!--[if gte mso 9]>
+			<v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+				<v:fill type="tile" color="#ffffff"></v:fill>
+			</v:background>
+		<![endif]-->
+      <table width="100%" cellspacing="0" cellpadding="0" class="es-wrapper">
+        <tbody>
+          <tr>
+            <td valign="top" class="esd-email-paddings">
+              <table cellpadding="0" cellspacing="0" align="center" class="esd-header-popover es-header">
+                <tbody>
+                  <tr>
+                    <td align="center" class="esd-stripe">
+                      <table bgcolor="#ffffff" align="center" cellpadding="0" cellspacing="0" width="600" class="es-header-body">
+                        <tbody>
+                          <tr>
+                            <td align="left" class="es-p20t es-p20r es-p20l esd-structure">
+                              <table cellpadding="0" cellspacing="0" width="100%">
+                                <tbody>
+                                  <tr>
+                                    <td width="560" valign="top" align="center" class="es-m-p0r esd-container-frame">
+                                      <table cellpadding="0" cellspacing="0" width="100%">
+                                        <tbody>
+                                          <tr>
+                                            <td align="center" class="esd-block-image" style="font-size:0px">
+                                              <a target="_blank" href="https://viewstripo.email">
+                                                <img src="https://tlr.stripocdn.email/content/guids/CABINET_0d71d49034ae71e9fc9c6ea70677feb4/images/group_90.png" alt="Logo" height="50" title="Logo" style="display:block">
+                                              </a>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <table cellspacing="0" cellpadding="0" align="center" class="es-content">
+                <tbody>
+                  <tr>
+                    <td align="center" bgcolor="#F3E2D8" class="esd-stripe" style="background-color:#f3e2d8">
+                      <table width="600" cellspacing="0" cellpadding="0" bgcolor="#F3E2D8" align="center" class="es-content-body" style="background-color:#f3e2d8">
+                        <tbody>
+                          <tr>
+                            <td align="left" class="esd-structure es-p40t es-p40b es-p20r es-p20l">
+                              <table width="100%" cellspacing="0" cellpadding="0">
+                                <tbody>
+                                  <tr>
+                                    <td width="560" valign="top" align="center" class="es-m-p0r es-m-p20b esd-container-frame">
+                                      <table width="100%" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                          <tr>
+                                            <td align="center" class="esd-block-image" style="font-size:0px">
+                                              <a target="_blank" href="https://viewstripo.email">
+                                                <img src="https://tlr.stripocdn.email/content/guids/CABINET_0d71d49034ae71e9fc9c6ea70677feb4/images/group_89_Ouc.png" alt="" width="515" class="adapt-img" style="display:block">
+                                              </a>
+                                            </td>
+                                          </tr>
+                                          <tr>
+                                            <td align="center" class="esd-block-text es-p30t">
+                                              <h1>
+                                                Turn your ideas into reality with us
+                                              </h1>
+                                            </td>
+                                          </tr>
+                                          <tr>
+                                            <td align="center" class="esd-block-text es-p20t es-p20b">
+                                              <p>
+                                                Hi! Welcome! Thanks for joining!
+                                              </p>
+                                            </td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </body>
+</html>
+`
